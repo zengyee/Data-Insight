b5add4c021c0ba6a7d41515627539c3f5dabece6
zengyee <zengyee@hotmail.com>
2016-03-17 13:22:28 +0800

    Update views

--- ETL/21vTicket/StagingDb/Views/v_CSS_Hop_Base.sql
+++ ETL/21vTicket/StagingDb/Views/v_CSS_Hop_Base.sql
@@ -1,4 +1,4 @@
 ﻿CREATE VIEW [dbo].[v_CSS_Hop_Base]
-	AS SELECT distinct [Case_ID], [AssignedTo] FROM [dbo].[v_CSS_Data_Detail]
+	AS SELECT distinct [Case_ID], [CurrentOwner_ID] FROM [dbo].[v_CSS_Data_Detail]
 
 
